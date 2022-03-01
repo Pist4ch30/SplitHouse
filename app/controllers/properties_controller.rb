a35d3d2301ee519bdd4fe0198fb7a161a5f46fa6
Alexandre Meline <meline.alexandre@laposte.net>
2022-03-01 15:26:11 +0100

    Create controller => Manque controller Parts

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,19 @@
+class PropertiesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
