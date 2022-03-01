70af234f9cd82fcad0348c26389f162fe3215bd4
Alexandre Meline <meline.alexandre@laposte.net>
2022-03-01 16:29:56 +0100

    Update => Routing > Nested Bookings in properties

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :favorites, only:[:new, :create, :update, :destroy, :index, :show]
+
   resources :properties, only:[:new, :create, :update, :destroy, :index, :show] do
     resources :parts, only:[:new, :create, :update, :destroy]
     resources :bookings, only: [:create, :new, :update, :destroy, :index, :show]
