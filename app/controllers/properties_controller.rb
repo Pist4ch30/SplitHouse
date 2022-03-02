284b14a31ef5ea758032941d300fa74ce6198f4c
Olivier Raimbault <o.raimbault@gmail.com>
2022-03-02 16:00:22 +0100

    property index v1

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -12,6 +12,15 @@ class PropertiesController < ApplicationController
   end
 
   def index
+    @properties = Property.all
+    @markers = @properties.geocoded.map do |property|
+      {
+        lat: property.latitude,
+        lng: property.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { property: property }),
+        image_url: helpers.asset_url("house.png")
+      }
+    end
   end
 
   def show
