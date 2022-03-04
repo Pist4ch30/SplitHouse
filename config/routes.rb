b7019645aa50bdc6c81f9608263ba3fc4115574a
Alexandre Meline <meline.alexandre@laposte.net>
2022-03-04 10:46:51 +0100

    Create Bagde => Cards Prtoperties Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :favorites, only:[:new, :create, :update, :destroy, :index, :show]
   get 'dashboard', to: 'dashboards#dashboard'
-  resources :properties, only:[:new, :create, :update, :destroy, :index, :show] do
-    member do
-      post :favorite
-      delete :unfavorite
-    end
+  get 'dashboard/agenda', to: 'dashboards#agenda'
+  get 'dashboard/renting', to: 'dashboards#renting'
 
+  resources :properties, only:[:new, :create, :update, :destroy, :index, :show] do
     resources :parts, only:[:new, :create, :update, :destroy]
-    resources :bookings, only:[:create, :new, :update, :destroy, :index, :show]
+    resources :bookings, only: [:create, :new, :update, :destroy, :index, :show]
   end
 end
